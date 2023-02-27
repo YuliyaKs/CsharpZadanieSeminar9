f43238f006aa2a42ffbf24958275c5c718ca3948
Yuliya_K <live2do@yandex.ru>
2023-02-27 12:56:02 +0300

    add Задача 64. Числа от N до 1

---
+++ 64/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 64. Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от N до 1. 
+// Выполнить с помощью рекурсии.
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите значение N: ");
+int n = int.Parse(ReadLine());
+
+WriteLine(PrintNumbersNto1(n));
+
+string PrintNumbersNto1(int number)
+{
+    if (number == 1)
+    {
+        return number.ToString();
+    }
+    string s = number.ToString() + " " + PrintNumbersNto1(number - 1);
+    return (s);
+}
