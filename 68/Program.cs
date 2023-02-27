beb66d583c64de0f0c0601a3a0c01ac9687402c6
Yuliya_K <live2do@yandex.ru>
2023-02-27 13:59:49 +0300

    add Задача 68. Функция Аккермана

---
+++ 68/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 68. Напишите программу вычисления функции Аккермана 
+// с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+using System;
+using static System.Console;
+
+Clear();
+Write("Введите число N: ");
+double n = double.Parse(ReadLine());
+Write("Введите число M: ");
+double m = double.Parse(ReadLine());
+
+WriteLine(Ackermann(n, m));
+
+double Ackermann(double n, double m)
+{
+    double result = 0;
+    if (n == 0)
+        result = m + 1;
+    else if (n != 0 && m == 0)
+        result = Ackermann(n - 1, 1);
+    else
+        result = Ackermann(n - 1, Ackermann(n, m - 1));
+    return result;
+}
